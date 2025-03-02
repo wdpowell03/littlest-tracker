fea37d183e067d8a7dbc6eaa904c6fece0759b18
wdpowell03 <wdpowell03@gmail.com>
2025-03-02 11:20:34 -0500

    added id

--- googleSheets.js
+++ googleSheets.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 const fetchSheetData = async () => {
-  const sheetId = 'your-sheet-id'; 
-  const apiKey = 'your-api-key';   
-  const range = 'Sheet1!A1:D10';   
+  const sheetId = '1Bzme5HbQjKD1qa2-qfRHGMqpiE58NxgGNNVXATaA_BM';
+  const apiKey = 'AIzaSyBjGRDGH8J-8JHZPMYdblH5m0O2TwNxn_M';
+  const range = '';
 
   try {
     const response = await axios.get(
       `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
     );
-    
+
     const data = response.data.values;  // This is the data from your Google Sheet
     console.log(data);
-    
+
     // You can now set this data in your state or render it in your component
   } catch (error) {
     console.error('Error fetching sheet data:', error);
